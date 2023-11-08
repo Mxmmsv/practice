ed57630f329684b701cafcb30b53d53f1ebdf317
Mxmmsv <oikioikioikiabrams@gmail.com>
2023-11-08 22:11:13 +0300

    feat: complete another task

--- app.js
+++ app.js
@@ -1,56 +1,5 @@
-/*
-	Пользователь хочет приобрести игру в магазине
-	Он может это сделать отлько если:
-	- Eго баланс больше 1000 (balance) 
-	или число бонусов больше 100 (bonusBalance)
-	- Он не забанен (isBanned)
-	- Игра не кулена (isExist)
-	- Игра в продаже (isSelling)
-	Напишите условие для покупки и выведите в консоль
-	результат
-*/
+/* Переписать функцию в стрелочную */
+const toPower = (num, power) => num ** power;
 
-let balance = prompt('Введите число на балансе')
-let bonusBalance = prompt('Введите число бонусов')
-let isBanned = prompt('Вы забанены?')
-switch (isBanned) {
-    case 'да':
-        isBanned = true;
-        break;
-    case 'нет':
-        isBanned = false;
-        break;
-    default:
-        isBanned = false;
-        break;
-}
+console.log(toPower(2, 3));
 
-let isExist = prompt('Игра куплена?')
-switch (isExist) {
-    case 'да':
-        isExist = true;
-        break;
-    case 'нет':
-        isExist = false;
-        break;
-    default:
-        isExist = false;
-        break;
-}
-
-let isSelling = prompt('Игра продаётся?')
-switch (isSelling) {
-    case 'да':
-        isSelling = true;
-        break;
-    case 'нет':
-        isSelling = false;
-        break;
-    default:
-        isSelling = false;
-        break;
-}
-
-let capacity = (balance >= 1000 || bonusBalance >= 100) && !isBanned && !isExist && isSelling;
-
-console.log(`Вася хочет купить игру, и он ${capacity === true ? 'сможет' : 'не сможет'} сделать это!`)
